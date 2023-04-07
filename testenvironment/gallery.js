a6d14002d6bafe7a4ebc8aac60113cdf246d6793
Estamir <129910755+Estamir@users.noreply.github.com>
2023-04-07 03:54:25 -0400

    Update gallery.js

--- testenvironment/gallery.js
+++ testenvironment/gallery.js
@@ -1,15 +1,15 @@
-const url = "pages.json";
+const url = "./pages.json";
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
     const container = document.getElementById("gallery-container");
-    for (let i = 0; i < data.pages.length; i++) {
-      const page = data.pages[i];
+    data.pages.forEach(page => {
       if (page.tags.includes("butt")) {
         const thumbnail = document.createElement("img");
-        thumbnail.src = page.thumbnail;
+        thumbnail.src = page.thumbnailUrl;
         thumbnail.alt = page.title;
+        thumbnail.classList.add("gallery-thumbnail");
         const title = document.createElement("p");
         title.innerText = page.title;
         const link = document.createElement("a");
@@ -18,9 +18,8 @@ fetch(url)
         link.appendChild(title);
         container.appendChild(link);
       }
-    }
+    });
   })
   .catch(error => {
     console.error("Error fetching pages:", error);
   });
-
